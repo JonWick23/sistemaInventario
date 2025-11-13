9650aa82f62d4740278d5caab99e52aac20060fd
diegolopezbarron <diego11lopez20013@gmail.com>
2025-11-13 09:59:19 -0600

    Add files via upload
    menu

---
+++ menu.php
@@ -0,0 +1,15 @@
+<html>
+    <link rel="stylesheet" type="text/css" media="screen" href="vent.css">
+    <header>
+        <nav class="nav">
+            <div class="logo">VENTAS</div>
+            <ul class="menu">
+                <li><a href="#">INICIO</a></li>
+                <li><a href="#">INVENTARIO</a></li>
+                <li><a href="compras.html">COMPRAS</a></li>
+                <li><a href="#">PROVEEDORES</a></li>
+                <li><a href="#">ACERCA DE</a></li>
+            </ul>
+        </nav>
+    </header>
+</html>
